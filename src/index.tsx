f21654cbb1da4974563cf523cca69a36ea3af9bf
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-04-02 19:09:06 -0300

    configuring seeds in miragejs and list transactions from the api in transactions table

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,30 @@ createServer({
   models: {
     transaction: Model
   },
-  
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createAt: new Date('2021-04-02 19:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createAt: new Date('2021-04-02 20:00:00')
+        }
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
