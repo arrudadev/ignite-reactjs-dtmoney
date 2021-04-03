b0514b63b139fea88bded560086eaee591b8a119
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-04-02 21:29:13 -0300

    calculating summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+
 import incomeSVG from '../../assets/income.svg';
 import outcomeSVG from '../../assets/outcome.svg';
 import totalSVG from '../../assets/total.svg';
@@ -5,6 +9,24 @@ import totalSVG from '../../assets/total.svg';
 import { Container } from "./styles";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce((accumulator, transaction) => {
+    if (transaction.type === 'deposit') {
+      accumulator.deposits += transaction.amount;
+      accumulator.total += transaction.amount;
+    } else {
+      accumulator.withdraws += transaction.amount;
+      accumulator.total -= transaction.amount;
+    }
+    
+    return accumulator;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  });
+
   return (
     <Container>
       <div>
@@ -12,7 +34,13 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeSVG} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.deposits)
+          }
+        </strong>
       </div>
 
       <div>
@@ -20,7 +48,14 @@ export function Summary() {
           <p>Saidas</p>
           <img src={outcomeSVG} alt="Saidas"/>
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          - 
+          {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.withdraws)
+          }
+        </strong>
       </div>
 
       <div className="highlight-background">
@@ -28,7 +63,13 @@ export function Summary() {
           <p>Total</p>
           <img src={totalSVG} alt="Total"/>
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.total)
+          }
+        </strong>
       </div>
     </Container>
   );
